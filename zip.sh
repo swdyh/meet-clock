9c84e8724c855409bf70954a2590c2d1bb7418a5
swdyh <youhei@gmail.com>
2022-05-22 13:05:57 +0900

    add zip script

---
+++ zip.sh
@@ -0,0 +1,13 @@
+file=meet-clock.zip
+zip $file \
+LICENSE \
+README.md \
+content_meet.js \
+manifest.json \
+meet-clock-icon128.png \
+meet-clock-icon16.png \
+meet-clock-icon32.png \
+meet-clock-icon48.png \
+
+echo
+echo $file
